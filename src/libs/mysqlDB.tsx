d6b1711e7c8a9ee34dd3661af91773b15989d0d5
Naidelyn Maldonado <naidelynmaldonado54@gmail.com>
2024-03-29 14:47:15 -0600

    prueba

--- src/libs/mysqlDB.tsx
+++ src/libs/mysqlDB.tsx
@@ -2,7 +2,7 @@ import mysql from "serverless-mysql"
 
 export const connGlobal = mysql({
     config: {
-        host     : "193.203.166.107",
+        host     : "srv1242.hstgr.io",
         user     : "u966946366_inhands",
         password : "inyellowes01.A",
         database : "u966946366_inhands",
